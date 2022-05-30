1e7df6c276ca8df6a2e9e142463ac6cefb4ec220
bintou <fofana_bintou@outlook.fr>
2022-05-30 21:02:10 +0200

    Fermeture du modal et ajustement de l'image pour les grands écrans

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,6 +15,10 @@ const formData = document.querySelectorAll(".formData");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+function close_modal(){
+  modalbg.style.display="none";
+}
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
